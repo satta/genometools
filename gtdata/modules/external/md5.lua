49ca919c464230d0901a4a47cc99f3d1cfa452cf
Dirk Willrodt <willrodt@zbh.uni-hamburg.de>
2016-07-11 17:35:32 +0200

    remove old md5 files, work on test for md5 lua

--- gtdata/modules/external/md5.lua
+++ gtdata/modules/external/md5.lua
@@ -2,14 +2,22 @@
 -- $Id: md5.lua,v 1.4 2006/08/21 19:24:21 carregal Exp $
 ----------------------------------------------------------------------------
 
-local core = require"md5.core"
-local string = require"string"
+local core
+local string = string or require"string"
 local pairs  = _G.pairs
 local assert = _G.assert
 local md5 = _G.md5
 
-module ("md5")
+if string.find(_VERSION, "Lua 5.0") then
+	local cpath = os.getenv"LUA_CPATH" or "/usr/local/lib/lua/5.0/"
+	core = loadlib(cpath.."md5/core.so", "luaopen_md5_core")()
+else
+	core = require"md5.core"
+end
 
+local pairs  = _G.pairs
+local assert = _G.assert
+local md5 = _G.md5
 -- export md5.core to md5
 for k, v in pairs(core) do
   if k ~= "_M" and k ~= "_NAME" and k~= "_PACKAGE" then
@@ -22,9 +30,11 @@ end
 -- @param k String with original message.
 -- @return String with the md5 hash value converted to hexadecimal digits
 
-function sumhexa (k)
+function core.sumhexa (k)
   k = core.sum(k)
   return (string.gsub(k, ".", function (c)
            return string.format("%02x", string.byte(c))
          end))
 end
+
+return core
